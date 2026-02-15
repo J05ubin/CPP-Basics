6bea224d59beef3f61567bbce863aa264a364b9c
J <147149509+J05ubin@users.noreply.github.com>
2026-02-15 21:58:56 +0530

    Created a  simple calculator

---
+++ Input-Output/simple_calculator.cpp
@@ -0,0 +1,26 @@
+// Program: Simple calculator using user input
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    if(op == '+') cout << "Result: " << a + b;
+    else if(op == '-') cout << "Result: " << a - b;
+    else if(op == '*') cout << "Result: " << a * b;
+    else if(op == '/') cout << "Result: " << a / b;
+    else cout << "Invalid operator";
+
+    return 0;
+}
